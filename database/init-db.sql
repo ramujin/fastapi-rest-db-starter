4d6c5eeb4fd1a4c9d8a9bb3f8b93400e0a6f7594
Ramsin Khoshabeh <ramsin102@gmail.com>
2023-02-15 21:04:34 -0800

    small changes for clarity part 3

--- database/init-db.sql
+++ database/init-db.sql
@@ -7,8 +7,8 @@ drop table if exists users;
 
 -- 1. Create the users table
 create table if not exists users (
-  [INSERT CODE HERE]
+  -- [INSERT CODE HERE]
 );
 
 -- 2. Insert initial seed records into the table
-[INSERT CODE HERE]
+-- [INSERT CODE HERE]
